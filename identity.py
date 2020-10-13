e55a2997b5eb1d8aa6ba2935712f290d5971aded
yangwang <49030340+yangwang@users.noreply.github.com>
2020-10-13 22:28:10 +0800

    Update identity.py
    +输入检测、星座、生肖

--- identity.py
+++ identity.py
@@ -1,35 +1,36 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Python version 2.7.13 or 3.7.2
-
-import random
 import re
-# 导入某个模块的部分类或方法
 from datetime import datetime, timedelta
-
-# 导入常量并重命名
-import constant as const
+import data
 
 
-class IdNumber(str):
+def check_input():
+    while True:
+        id_number = input('请输入身份证: ')
+        if re.match(data.ID_NUMBER_18_REGEX, id_number):
+            return id_number
+            break
+        else:
+            print('输入错误！')
 
+
+class Id:
+    """分析18位身份证信息"""
     def __init__(self, id_number):
-        super(IdNumber, self).__init__()
         self.id = id_number
-        self.area_id = int(self.id[0:6])
-        self.birth_year = int(self.id[6:10])
-        self.birth_month = int(self.id[10:12])
-        self.birth_day = int(self.id[12:14])
+        self.area_id = int(id_number[0:6])
+        self.birth_year = int(id_number[6:10])
+        self.birth_month = int(id_number[10:12])
+        self.birth_day = int(id_number[12:14])
 
-    def get_area_name(self):
+    def area_name(self):
         """根据区域编号取出区域名称"""
-        return const.AREA_INFO[self.area_id]
+        return data.AREA_INFO[self.area_id]
 
-    def get_birthday(self):
+    def birthday(self):
         """通过身份证号获取出生日期"""
         return "{0}-{1}-{2}".format(self.birth_year, self.birth_month, self.birth_day)
 
-    def get_age(self):
+    def age(self):
         """通过身份证号获取年龄"""
         now = (datetime.now() + timedelta(days=1))
         year, month, day = now.year, now.month, now.day
@@ -42,52 +43,55 @@ class IdNumber(str):
             else:
                 return year - self.birth_year
 
-    def get_sex(self):
+    def sex(self):
         """通过身份证号获取性别， 女生：0，男生：1"""
-        return int(self.id[16:17]) % 2
+        if int(self.id[16:17]) % 2 == 0:
+            return '女'
+        else:
+            return '男'
 
-    def get_check_digit(self):
-        """通过身份证号获取校验码"""
+    def verify(self):
+        """校验身份证是否正确"""
         check_sum = 0
         for i in range(0, 17):
             check_sum += ((1 << (17 - i)) % 11) * int(self.id[i])
         check_digit = (12 - (check_sum % 11)) % 11
-        return check_digit if check_digit < 10 else 'X'
+        if check_digit == 10:
+            check_digit = 'X'
 
-    @classmethod
-    def verify_id(cls, id_number):
-        """校验身份证是否正确"""
-        if re.match(const.ID_NUMBER_18_REGEX, id_number):
-            check_digit = cls(id_number).get_check_digit()
-            return str(check_digit) == id_number[-1]
+        if str(check_digit) == self.id[-1]:
+            return '正确'
         else:
-            return bool(re.match(const.ID_NUMBER_15_REGEX, id_number))
+            return '错误'
 
-    @classmethod
-    def generate_id(cls, sex=0):
-        """随机生成身份证号，sex = 0表示女性，sex = 1表示男性"""
+    def constellation(self):
+        """获取星座信息"""
+        month = self.birth_month
+        day = self.birth_day
 
-        # 随机生成一个区域码(6位数)
-        id_number = str(random.choice(list(const.AREA_INFO.keys())))
-        # 限定出生日期范围(8位数)
-        start, end = datetime.strptime("1960-01-01", "%Y-%m-%d"), datetime.strptime("2000-12-30", "%Y-%m-%d")
-        birth_days = datetime.strftime(start + timedelta(random.randint(0, (end - start).days + 1)), "%Y%m%d")
-        id_number += str(birth_days)
-        # 顺序码(2位数)
-        id_number += str(random.randint(10, 99))
-        # 性别码(1位数)
-        id_number += str(random.randrange(sex, 10, step=2))
-        # 校验码(1位数)
-        return id_number + str(cls(id_number).get_check_digit())
+        start_date = data.get_constellation()[month]['start_date']
+        start_day = int(start_date.split('-')[-1])
+
+        if day < start_day:
+            tmp_month = 12 if month - 1 == 0 else month - 1
+            return data.get_dataellation()[tmp_month]['name']
+
+        return data.get_constellation()[month]['name']
+
+    def chinese_zodiac(self):
+        """获取生肖"""
+        start = 1900  # 子鼠
+        end = self.birth_year
+        key = (end - start) % 12
+        return data.get_chinese_zodiac()[key]
 
 
 if __name__ == '__main__':
-    random_sex = random.randint(0, 1)  # 随机生成男(1)或女(0)
-    print(IdNumber.generate_id(random_sex))  # 随机生成身份证号
-    print(IdNumber('410326199507103197').area_id)  # 地址编码:410326
-    print(IdNumber('410326199507103197').get_area_name())  # 地址:河南省洛阳市汝阳县
-    print(IdNumber('410326199507103197').get_birthday())  # 生日:1995-7-10
-    print(IdNumber('410326199507103197').get_age())  # 年龄:23(岁)
-    print(IdNumber('410326199507103197').get_sex())  # 性别:1(男)
-    print(IdNumber('410326199507103197').get_check_digit())  # 校验码:7
-    print(IdNumber.verify_id('410326199507103198'))  # 检验身份证是否正确:False
+    input_id = check_input()
+    print('地址:', Id(input_id).area_name())
+    print('生日:', Id(input_id).birthday())
+    print('年龄:', Id(input_id).age())
+    print('性别:', Id(input_id).sex())
+    print('校验:', Id(input_id).verify())
+    print('星座:', Id(input_id).constellation())
+    print('生肖:', Id(input_id).chinese_zodiac())
